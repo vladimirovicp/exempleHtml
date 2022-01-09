25c59680f307b17642a3fdb66b6db61bb8d381f7
vladimirovicp <vladimirovicp70@gmail.com>
2022-01-09 23:20:48 +0400

    feat: rezult burger

--- burger/js/script.js
+++ burger/js/script.js
@@ -45,3 +45,50 @@ if (isMobile.any()){
 } else {
     document.body.classList.add('_pc');
 } // Добавили класс в body, для ПК _pc
+
+
+//Меню бургер
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if(iconMenu){
+    iconMenu.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+
+}
+
+
+
+//Прокрутка при клике
+let menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            // const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offserHeight;
+
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            if(iconMenu.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+
+            e.preventDefault();
+        }
+    }
+}
