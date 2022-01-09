74e639aff49a68c331db6d6a2ae0706492dccbe9
vladimirovicp <vladimirovicp70@gmail.com>
2022-01-09 15:46:59 +0400

    feat: arrow _touch

--- burger/js/script.js
+++ burger/js/script.js
@@ -30,6 +30,18 @@ var isMobile = {
 
 if (isMobile.any()){
     document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+
+    if( menuArrows.length > 0){
+        for(let index = 0; index < menuArrows.length; index++){
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function(e){
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+
 } else {
     document.body.classList.add('_pc');
 } // Добавили класс в body, для ПК _pc
