11f118d0398f59b8dd7a95694cd0ba2dad59c033
vladimirovicp <vladimirovicp70@gmail.com>
2022-01-31 12:16:54 +0400

    feat: video player start

---
+++ Custom-HTML5-Video-Player/v2/assets/js/video-player.js
@@ -0,0 +1,99 @@
+// /*  Получить элементы */
+
+
+const player = document.querySelector('.player');
+const playerVideo = player.querySelector('.player__video');
+const layerControls = player.querySelector('.layer__controls');
+
+const video = player.querySelector('.viewer');
+
+// const progress = player.querySelector('.progress');
+// const progressBar = player.querySelector('.progress__filed');
+
+const toggle = player.querySelector('.toggle');
+
+console.log(video)
+
+// const skipButton = player.querySelectorAll('[data-skip]')
+// const ranges = player.querySelectorAll('.player__slider')
+
+
+
+// Скрыть собственные элементы управления
+// Первое, что нам нужно сделать, это скрыть элементы управления видео по умолчанию и предоставить наш собственный интерфейс, 
+// как только будет определено, что браузер поддерживает видео HTML5.
+
+const videoWorks = !!document.createElement('video').canPlayType;
+//console.log(videoWorks)
+
+// canPlayTypeСвойство заключается в том, как мы можем обнаружить поддержку видеоформата в браузере. Чтобы использовать его, нам нужно 
+// создать экземпляр <video>элемента и проверить, поддерживает ли он canPlayTypeметод. Если это так, можно с уверенностью предположить, 
+// что видео HTML5 поддерживается, поэтому элементы управления по умолчанию быстро отключаются в пользу наших пользовательских элементов управления.
+
+if (videoWorks) {
+    player.controls = false;
+    layerControls.classList.remove('hidden');
+}
+
+
+
+// /* Посмтроить функции */
+
+function togglePlay() {
+
+    console.log('123')
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+// function updateButton(){
+//     const icon = this.paused ? '►' : '❚ ❚';
+//     toggle.textContent = icon;
+// }
+
+// function skip(){
+//     //console.log(this.dataset.skip)
+//     video.currentTime += parseFloat(this.dataset.skip);
+// }
+
+// function handleRangeUpdate(){
+//     // console.log(this.name)
+//     // console.log(this.value)
+//     video[this.name] = this.value;
+// }
+
+// // обрабатываем прогресс
+// function handleProgress(){
+//     const percent = (video.currentTime / video.duration) * 100;
+//     progressBar.style.flexBasis = `${percent}%`;
+// }
+
+// function scrub(e){
+
+//     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+//     video.currentTime = scrubTime;
+
+// }
+
+
+// /* Включим */
+
+video.addEventListener('click', togglePlay);
+
+// video.addEventListener('play', updateButton);
+// video.addEventListener('pause', updateButton);
+// //слушаем событие обновление времени
+// video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+// skipButton.forEach(skipButton => skipButton.addEventListener('click', skip));
+// ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
+// ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
+
+// let mousedown = false;
+// progress.addEventListener('click',scrub);
+
+// progress.addEventListener('mousedown', (e) => mousedown && scrub(e));
+// progress.addEventListener('mousedown', () => mousedown = true);
+// progress.addEventListener('mouseup', () => mousedown = false);
