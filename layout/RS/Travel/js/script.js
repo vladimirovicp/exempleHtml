5d19fb0958cf652f6a210c7ce2591eba704b4ea8
vladimirovicp <vladimirovicp70@gmail.com>
2022-05-27 23:54:33 +0400

    add: slider

--- layout/RS/Travel/js/script.js
+++ layout/RS/Travel/js/script.js
@@ -1,33 +1,14 @@
 const swiper = new Swiper('.destinations__swiper', {
-    // Optional parameters
-    // direction: 'vertical',
     loop: true,
-    slidesPerView: 2,
-    // If we need pagination
+    slidesPerView: 1.68,
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
 
     centeredSlides: true,
     watchSlidesVisibility: true,
 
-
-
-
-
-    // Navigation arrows
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
-
-    // loopedSlides: 3,
-
-//    Свободнфй режим
+   // Свободнфй режим
     freeMode: true,
 });
